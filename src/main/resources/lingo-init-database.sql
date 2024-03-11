8f92a589eb7c4a7f700d77fbe0f1fde51e1be648
Muneeb Javed <muneebbutt444@gmail.com>
2024-03-11 22:27:13 +0500

    Update LingoCraft Database User To Postgres

--- src/main/resources/lingo-init-database.sql
+++ src/main/resources/lingo-init-database.sql
@@ -1,13 +1,8 @@
 -- database creation
-CREATE ROLE lingo_owner WITH PASSWORD 'lingo_owner';
-ALTER ROLE lingo_owner WITH LOGIN;
-
-CREATE ROLE lingo_read WITH PASSWORD 'lingo_read';
-ALTER ROLE lingo_read WITH LOGIN;
 
 CREATE
     DATABASE "lingo_craft"
-    WITH OWNER = lingo_owner
+    WITH OWNER = postgres
     ENCODING = 'UTF8'
     CONNECTION LIMIT = -1
     IS_TEMPLATE = False;
