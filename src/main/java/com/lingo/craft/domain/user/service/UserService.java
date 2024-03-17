7c266943e910dcf389fd06ee6b0bde829aaecee5
Aqib Javed <aqib.javed@diabolocom.com>
2024-03-17 08:51:59 +0000

    86enw1u90 | User get by id API

--- src/main/java/com/lingo/craft/domain/user/service/UserService.java
+++ src/main/java/com/lingo/craft/domain/user/service/UserService.java
@@ -1,8 +1,11 @@
 package com.lingo.craft.domain.user.service;
 
+import com.lingo.craft.domain.user.exception.InvalidUserIdException;
 import com.lingo.craft.domain.user.exception.UserCreationException;
+import com.lingo.craft.domain.user.exception.UserNotFoundException;
 import com.lingo.craft.domain.user.model.UserModel;
 import com.lingo.craft.domain.user.ports.outbound.UserRepository;
+import java.util.UUID;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
@@ -13,10 +16,40 @@ public class UserService {
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
+
+    public UserModel getUserById(String id) {
+        try {
+            var uuid = UUID.fromString(id);
+            var optionalUser = userRepository.getById(uuid);
+
+            if (optionalUser.isEmpty()) {
+                throw new UserNotFoundException(
+                    HttpStatus.NOT_FOUND,
+                    String.format(
+                        "User not found against id = {%s}",
+                        id
+                    )
+                );
+            }
+
+            return optionalUser.get();
+
+        } catch (IllegalArgumentException ex) {
+            throw new InvalidUserIdException(
+                HttpStatus.BAD_REQUEST,
+                String.format(
+                    "User id {%s} is not in UUID format",
+                    id
+                ),
+                ex
+            );
+        }
+    }
+
     public UserModel createUser(UserModel user) {
         var optionalUser = userRepository.create(user);
 
-        if(optionalUser.isEmpty()) {
+        if (optionalUser.isEmpty()) {
             throw new UserCreationException(
                 HttpStatus.INTERNAL_SERVER_ERROR,
                 "Error while creating user in database"
